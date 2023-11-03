93d66a70d8304360fc54a4b2b8b9f437f5cd30f1
Stephane Nowicki <stephane.nowicki@yahoo.fr>
2023-11-03 17:50:16 +0100

    Modification pour récupérer les travaux depuis l'API en ajoutant une fonction asynchrone main

--- FrontEnd/assets/JS/script.js
+++ FrontEnd/assets/JS/script.js
@@ -1,24 +1,24 @@
 
 function afficherWorks(works) {
+        const gallery = document.querySelector(".gallery");
+        gallery.innerHTML = "";
+        // Récupération de l'élément du DOM qui accueillera la galerie //
         for (let i = 0; i < works.length; i++) {
                 const work = works[i];
-                
-                // Récupération de l'élément du DOM qui accueillera la galerie //
-                const gallery = document.querySelector(".gallery");
-                
+
                 // Création d'une balise dédiée à un travail //
                 const workElement = document.createElement("figure");
-                
+
                 // Création des balises //
                 const imageElement = document.createElement("img");
                 // Mettre à jour l'image afichée dans l'élément HTML //
                 imageElement.src = work.imageUrl;
                 const figcaption = document.createElement("figcaption");
                 figcaption.innerText = work.title;
-                
+
                 // On rattache la balise workElement à la div gallery //
                 gallery.appendChild(workElement);
-                
+
                 // Ajout des éléments au DOM //   
                 workElement.appendChild(imageElement);
                 workElement.appendChild(figcaption);
@@ -28,53 +28,71 @@ function afficherWorks(works) {
 
 
 function afficherCategories(categories, works) {
+        // Récupération de l'élément du DOM qui accueillera les catégories //
+        const filtres = document.querySelector(".filtres");
+
+        // Création du bouton "Tous" pour afficher tous les travaux //
+        const toutesLesCategories = document.createElement("button");
+        toutesLesCategories.classList.add("btn-filtre");
+        toutesLesCategories.innerHTML = "Tous";
+        filtres.appendChild(toutesLesCategories);
+
+        // Ajout gestionnaire d'événements pour le bouton "Tous" //
+        toutesLesCategories.addEventListener("click", function () {
+                afficherWorks(works); // Afficher tous les travaux //
+        });
+
         for (let i = 0; i < categories.length; i++) {
                 const categorie = categories[i];
-                
-                // Récupération de l'élément du DOM qui accueillera les catégories //
-                const filtres = document.querySelector(".filtres"); 
+
                 // Création d'une balise dédiée à un bouton filtre de la galerie //
                 const categoryElement = document.createElement("button");
-                console.log(categoryElement);
                 categoryElement.classList.add("btn-filtre");
                 // Récupération de la propriété name dans la catégorie du tableau //
                 categoryElement.innerHTML = categorie.name;
-                
-                // Créations des balises //
-                const filtre = document.createElement("div");
-                filtre.className = "filtre";
-                const tousLesFiltres = document.createElement("button");
-                tousLesFiltres.className = "btn-filtre-tous";
+
+                // Ajout gestionnaire d'événement pour chaque catégorie //
+                categoryElement.addEventListener("click", function () {
+                        const categoryName = categorie.name;
+                        const travauxFiltres = works.filter(function (work) {
+                                return work.category.name === categoryName;
+                        });
+
+                        // Afficher les travaux filtrés //
+                        afficherWorks(travauxFiltres);
+                });
+
                 // categoryElement et l'enfant de filtres //
                 filtres.appendChild(categoryElement);
-                
-                categoryElement.addEventListener("click", function () {
-                        const categoriesFiltrees = works.filter(function (work) {
-                                return work.category.name;
-                        })
-                        console.log(categoriesFiltrees);
-                        // Effacer les filtres précédents //
-                        document.querySelector(".filtres").innerHTML = "";
-                        // Afficher les catégories filtrées //
-                        afficherCategories(categoriesFiltrees)
-                })
+
         }
 }
 
+// function getCategoriesFromWorks(works) {
+//         let categories = [];
+//         for(let i = 0; i < works.length; i++) {
+//                 const work = works[i];
+//                 console.log(work)
+//                 categories.push(JSON.stringify(work.category));
+//         }
+        
+//         return [...new Set(categories)].map(category => JSON.parse(category))
+// }
 
-async function main () {
+async function main() {
         // Récupération des travaux depuis l' API //
         const reponse = await fetch("http://localhost:5678/api/works");
         const works = await reponse.json();
         console.log(works);
-        
+
         // Récupération des catégories depuis l'API //
         const reponse2 = await fetch("http://localhost:5678/api/categories");
         const categories = await reponse2.json();
+        // const categories = getCategoriesFromWorks(works);
         console.log(categories);
-        
+
         afficherWorks(works);
-        
+
         afficherCategories(categories, works);
 }
 main();
